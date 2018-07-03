6690ac36695052773e13aa374e63587bccf78d88
L-UOXIA <m17670840283@163.com>
2018-07-03 16:57:15 +0800

    zhangyifan

---
+++ webvx/pages/index/index.js
@@ -0,0 +1,21 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/sup1.png',
+      '/images/sup2.png'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+
+  },
+  
+  onLoad: function () {
+    
+  }
+})
